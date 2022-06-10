113062227f63287c299e4667608b29f7a0616c86
ogi <takahiro.ogi@gmail.com>
2022-06-10 16:08:31 +0900

    Add Text

---
+++ src/components/typography/text/Text.tsx
@@ -0,0 +1,63 @@
+import { SystemProps, Text as TextC, ThemeTypings } from "@chakra-ui/react";
+import { LegacyRef, ReactNode } from "react";
+import { PropsBase } from "../../../cores/types";
+
+interface IProps extends PropsBase {
+  /** @uxpinignoreprop */
+  uxpinRef: LegacyRef<HTMLDivElement>;
+  /**
+   * @uxpinpropname  Text
+   * @uxpincontroltype input
+   */
+  children: ReactNode;
+  fontSize: ThemeTypings["fontSizes"];
+  fontWeight: ThemeTypings["fontWeights"];
+  as:
+    | "i"
+    | "u"
+    | "abbr"
+    | "cite"
+    | "del"
+    | "em"
+    | "ins"
+    | "kbd"
+    | "mark"
+    | "s"
+    | "samp"
+    | "sub"
+    | "sup";
+  textAlign: "left" | "center" | "right";
+  casing: SystemProps["textTransform"];
+  colorScheme: ThemeTypings["colorSchemes"];
+  decoration: SystemProps["textDecoration"];
+  orientation: "horizontal" | "vertical";
+}
+
+/**
+ * @uxpindocurl https://v1.chakra-ui.com/docs/components/typography/text#changing-the-font-size
+ */
+export const Text = (props: IProps) => {
+  const {
+    as,
+    textAlign,
+    casing,
+    decoration,
+    orientation,
+    fontSize,
+    fontWeight,
+  } = props;
+  return (
+    <TextC
+      {...props}
+      as={as}
+      textAlign={textAlign}
+      casing={casing}
+      decoration={decoration}
+      orientation={orientation}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+    >
+      {props.children}
+    </TextC>
+  );
+};
---
+++ src/components/typography/text/presets/0-default.jsx
@@ -0,0 +1,8 @@
+import { defaultSx } from "../../../../cores/default-sx";
+import { Text } from "../Text";
+
+export default (
+  <Text uxpId="Text" sx={defaultSx}>
+    Text
+  </Text>
+);
