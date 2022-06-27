4aa0494ebde45f37a30f707fdca9ba47ed122b6b
ogi <takahiro.ogi@gmail.com>
2022-06-28 03:07:14 +0900

    [Table] Fix freeze browser when props are changed

--- src/components/table/presets/0-default.jsx
+++ src/components/table/presets/0-default.jsx
@@ -1,5 +1,5 @@
 import { Tbody } from "../../table-body";
-import { TableCaption } from "../../table-caption/TableCaption";
+import { TableCaption } from "../../table-caption/";
 import { Tfoot } from "../../table-foot";
 import { Thead } from "../../table-head";
 import { Td } from "../../table-td";
@@ -9,10 +9,8 @@ import { Table } from "../Table";
 
 export default (
   <Table uxpId="Table">
-    <TableCaption uxpId="TableChild-1">
-      Imperial to metric conversion factors
-    </TableCaption>
-    <Thead uxpId="TableHead">
+    <TableCaption uxpId="TableChildCaption">Table Caption</TableCaption>
+    <Thead uxpId="TableChildHead">
       <Tr uxpId="TableHeadTr">
         <Th uxpId="TableHeadTh-1">To convert</Th>
         <Th uxpId="TableHeadTh-2">into</Th>
@@ -29,22 +27,15 @@ export default (
           25.4
         </Td>
       </Tr>
-      <Tr uxpId="TableBodyTr-1">
+      <Tr uxpId="TableBodyTr-2">
         <Td uxpId="TableBodyTd-4">feet</Td>
         <Td uxpId="TableBodyTd-5">centimetres (cm)</Td>
         <Td uxpId="TableBodyTd-6" isNumeric>
           30.48
         </Td>
       </Tr>
-      <Tr uxpId="TableBodyTr-1">
-        <Td uxpId="TableBodyTd-7">yards</Td>
-        <Td uxpId="TableBodyTd-8">metres (m)</Td>
-        <Td uxpId="TableBodyTd-9" isNumeric>
-          0.91444
-        </Td>
-      </Tr>
     </Tbody>
-    <Tfoot uxpId="TableFoot">
+    <Tfoot uxpId="TableChildFoot">
       <Tr uxpId="TableFootTr">
         <Th uxpId="TableFootTh-1">To convert</Th>
         <Th uxpId="TableFootTh-2">into</Th>
